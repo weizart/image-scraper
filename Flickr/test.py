8b3095296c330b0f25719395ce7a95106d859106
Weizart <75230898+weizart@users.noreply.github.com>
2025-02-23 02:24:48 +0800

    Update test.py

--- Flickr/test.py
+++ Flickr/test.py
@@ -3,8 +3,8 @@ import urllib.request
 import os
 
 # Replace with your own Flickr API key and secret
-api_key = '2b58cb9e9948ddca307c5eeac7008aba'
-api_secret = 'Y002f592155836c09'
+api_key = ''
+api_secret = ''
 
 # 创建downloads目录
 os.makedirs('downloads', exist_ok=True)
